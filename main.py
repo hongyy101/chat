e603f1f19cd848ba8aff8e65d836b81056db620c
hongyy101 <243287285@qq.com>
2026-03-31 16:37:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -434,7 +434,7 @@ def get_local_ip():
     """获取本机局域网IP地址"""
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 9000))
+        s.connect(("0.0.0.0", 9000))
         ip = s.getsockname()[0]
         s.close()
         return ip
